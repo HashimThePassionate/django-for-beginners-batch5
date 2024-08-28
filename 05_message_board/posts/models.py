90740d7f53ab5de6eaf71960b962170084fd045c
HashimThePassionate <hashiimtahir@gmail.com>
2024-08-28 16:32:19 +0500

    table#ourborder@

---
+++ 05_message_board/posts/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title[:10]
---
+++ 05_message_board/posts/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from .views import PostList
+urlpatterns = [
+    path('', PostList.as_view(), name='home'),
+]
